b15eb0cb3a8af0ecfae2a1527019e04d76b8a09b
zhaomengmeng <zhaomengmengSX@163.com>
2020-09-30 16:40:45 +0800

    feat: setinterval&date

--- src/component/views/Father/inner/Son.js
+++ src/component/views/Father/inner/Son.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import m from '../../../material/material'
 
+
+class Tick extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      date: new Date().toLocaleTimeString()
+    }
+  }
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({
+        date: new Date().toLocaleTimeString()
+      })
+    },1000)
+  }
+  render(){
+    return(
+      <div>
+        <h4>hello,world!</h4>
+        <h5>it is {this.state.date}.</h5>
+      </div>
+    )
+  }
+}
+
 class Son extends React.Component{
   constructor(props){
     super(props);
@@ -25,6 +50,8 @@ class Son extends React.Component{
         </p>
         <m.Button variant="outlined" color="primary" onClick={() => {this.getFatherMethods()}}>子调用父的方法</m.Button>
         <m.Button variant="outlined" color="primary" onClick={() => {this.getFatherVal()}}>子获取父变量的值</m.Button>
+        <hr/>
+        <Tick/>
       </div>
     )
   }
