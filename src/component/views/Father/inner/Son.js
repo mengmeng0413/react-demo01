a3c986b486bdf223f7632d5bd18cb2717da7f0e6
zhaomengmeng <zhaomengmengSX@163.com>
2020-10-13 10:23:51 +0800

    feat: 元素变量

--- src/component/views/Father/inner/Son.js
+++ src/component/views/Father/inner/Son.js
@@ -67,6 +67,50 @@ function Greeting(props){
   return <GuestGreeting/>;
 }
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+  handleLoginClick(){
+    this.setState({isLoggedIn: true});
+  }
+  handleLogoutClick(){
+    this.setState({isLoggedIn: false});
+  }
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick}/>;
+    }else{
+      button = <LoginButton onClick={this.handleLoginClick}/>;
+    }
+    return(
+      <div>
+        <Greeting isLoggedIn={isLoggedIn}/>
+        {button}
+      </div>
+    )
+  }
+}
+
 class Son extends React.Component{
   constructor(props){
     super(props);
@@ -96,7 +140,7 @@ class Son extends React.Component{
         <hr/>
         <Toggle/>
         <hr/>
-        <Greeting isLoggedIn={true}/>
+        <LoginControl/>
       </div>
     )
   }
