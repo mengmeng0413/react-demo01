9e4a014423ca1dbb6309de77da686f48465f34f4
zhaomengmeng <zhaomengmengSX@163.com>
2020-11-30 15:55:38 +0800

    feat: 鼠标和指针事件

--- src/component/views/Practice/Practice.js
+++ src/component/views/Practice/Practice.js
@@ -300,6 +300,102 @@ class TryProp extends React.Component {
     }
 }
 
+class OuterClickExample extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { isOpen: false };
+        this.toggleContainer = React.createRef();
+
+        this.onClickHandler = this.onClickHandler.bind(this);
+        this.onClickOutsideHandler = this.onClickOutsideHandler.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('click', this.onClickOutsideHandler);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('click', this.onClickOutsideHandler);
+    }
+
+    onClickHandler() {
+        this.setState(currentState => ({
+            isOpen: !currentState.isOpen
+        }));
+    }
+
+    onClickOutsideHandler(event) {
+        if(this.state.isOpen && !this.toggleContainer.current.contains(event.target)){
+            this.setState({isOpen: false});
+        }
+    }
+
+    render() {
+        return (
+            <div ref = {this.toggleContainer}>
+                <button onClick={this.onClickHandler}>Select an Option</button>
+                {this.state.isOpen && (
+                    <ul>
+                        <li>Option 1</li>
+                        <li>Option 2</li>
+                        <li>Option 3</li>
+                    </ul>
+                )}
+            </div>
+        )
+    }
+}
+
+class BlurExample extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen:false };
+        this.timeOutId = null;
+         
+        this.onClickHandler = this.onClickHandler.bind(this);
+        this.onBlurHandler = this.onBlurHandler.bind(this);
+        this.onFocusHandler = this.onFocusHandler.bind(this);
+    }
+    onClickHandler() {
+        this.setState(currentState => ({
+            isOpen: !currentState.isOpen
+        }));
+    }
+
+    onBlurHandler() {
+        this.timeOutId = setTimeout(() => {
+            this.setState({
+                isOpen: false
+            });
+        });
+    }
+
+    onFocusHandler() {
+        clearTimeout(this.timeOutId);
+    }
+
+    render() {
+        return (
+            <div onBlur = {this.onBlurHandler}
+                 onFocus = {this.onFocusHandler}>
+                <button onClick = {this.onClickHandler}
+                        aria-haspopup = "true"
+                        aria-expanded = {this.state.isOpen}>
+                    Select an option
+                </button>
+                {this.state.isOpen && (
+                    <ul>
+                        <li>Option 1</li>
+                        <li>Option 2</li>
+                        <li>Option 3</li>
+                    </ul>
+                )}
+            </div>
+        )
+    }
+}
+
 class Practice extends React.Component {
     render() {
         let tags = ['111', '222', '333'];
@@ -345,6 +441,10 @@ class Practice extends React.Component {
                 <TimeOut />
                 <hr/>
                 <TryProp msg="give you msg"/>
+                <hr/>
+                <OuterClickExample />
+                <hr/>
+                <BlurExample />
             </div>
         )
     }
